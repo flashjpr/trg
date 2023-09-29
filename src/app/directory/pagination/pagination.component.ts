1e85e0f826c0371cfc132e01749586bd143e1674
flashjpr <flashjpr@gmail.com>
2023-09-29 10:05:35 +0300

    refactor(pagination): Optimize pagination display with limited page count

--- src/app/directory/pagination/pagination.component.ts
+++ src/app/directory/pagination/pagination.component.ts
@@ -10,6 +10,37 @@ export class PaginationComponent {
   @Input() pagination: Pagination | null = null;
   @Output() handleOnPageChange = new EventEmitter<number>();
 
+  readonly maxDisplayedPages: number = 3;
+
+  get startPage(): number {
+    if (this.pagination!.currentPage <= (this.maxDisplayedPages + 1) / 2) {
+      return 2;
+    }
+    if (this.pagination!.currentPage > this.totalPages - (this.maxDisplayedPages - 1) / 2) {
+      return this.totalPages - this.maxDisplayedPages + 1; // Again, exclude the last page
+    }
+    return this.pagination!.currentPage - Math.floor(this.maxDisplayedPages / 2);
+  }
+
+  get endPage(): number {
+    return Math.min(this.startPage + this.maxDisplayedPages - 1, this.totalPages - 1); // -1 to exclude the last page
+  }
+
+  get showFirstEllipsis(): boolean {
+    return this.startPage > 2; // always showing the first page
+  }
+
+  get showLastEllipsis(): boolean {
+    return this.endPage < this.totalPages - 1; // "-1" because we're always showing the last page
+  }
+
+  get displayedPages(): number[] {
+    let pages = [];
+    for (let i = this.startPage; i <= this.endPage; i++) {
+      pages.push(i);
+    }
+    return pages;
+  }
   get totalPages(): number {
     if (!!this.pagination?.totalItems && !!this.pagination?.itemsPerPage) {
       return Math.ceil(this.pagination.totalItems / this.pagination.itemsPerPage);
